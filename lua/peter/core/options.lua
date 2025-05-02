59464853879de0680671b03cffcb49356fe2f293
Peter Mazzocco <petermazzocco@gmail.com>
2025-05-02 13:00:22 -0700

    more

---
+++ lua/peter/core/options.lua
@@ -0,0 +1,34 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs
+opt.shiftwidth = 2 -- 2 spcaes for indent width
+opt.expandtab = true -- expand tab for spaces
+opt.autoindent = true -- copy indent from current line when starting a new one
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true --ignore case when seraching
+opt.smartcase = true -- if you include mixed case in search, assume to want case-sensitive
+
+opt.cursorline = true
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+--clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
